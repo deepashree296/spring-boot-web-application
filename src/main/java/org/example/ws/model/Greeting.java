185b40082125551530f23d6b016e57aafcbf27b2
Deepashree Kedia <kediadeepashree@gmail.com>
2018-04-13 15:15:05 +0530

    created a spring data jpa repository and stored greeting entity there. Also, made changes in service layer to call repository methods and removed data manipulation helper methods.

--- src/main/java/org/example/ws/model/Greeting.java
+++ src/main/java/org/example/ws/model/Greeting.java
@@ -1,29 +1,37 @@
 package org.example.ws.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+
+@Entity
 public class Greeting {
 
-    private Long id;
-    private String text;
+@Id
+@GeneratedValue
+private Long id;
+private String text;
 
-    public Greeting() {
+public Greeting() {
 
-    }
+}
 
-    public Long getId(){
-      return id;
-    }
+public Long getId(){
+        return id;
+}
 
-    public void setId(Long id){
-      this.id = id;
-    }
+public void setId(Long id){
+        this.id = id;
+}
 
-    public String getText(){
-      return text;
-    }
+public String getText(){
+        return text;
+}
 
-    public void setText(String text){
-      this.text = text;
-    }
+public void setText(String text){
+        this.text = text;
+}
 
 
 }
--- src/main/java/org/example/ws/service/GreetingServiceBean.java
+++ src/main/java/org/example/ws/service/GreetingServiceBean.java
@@ -5,100 +5,62 @@ import java.util.Map;
 import java.util.HashMap;
 
 import org.example.ws.service.GreetingService;
+import org.example.ws.repository.GreetingRepository;
 import org.example.ws.model.Greeting;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class GreetingServiceBean implements GreetingService {
 
-  // temporary helper methods to manage model object - a hard coded collection of greeting object. In actual, it will serve data from spring data repository
-  private static Long nextId;
-
-  // temporary data store
-  private static Map<Long, Greeting> greetingMap;
+@Autowired
+private GreetingRepository greetingRepository;
 
 
+// find all
+@Override
+public Collection<Greeting> findAll() {
+        Collection<Greeting> greetings = greetingRepository.findAll();
+        return greetings;
 
-  // find all
-  @Override
-  public Collection<Greeting> findAll() {
-    Collection<Greeting> greetings = greetingMap.values();
-    return greetings;
 
+}
 
-  }
+// find one
+@Override
+public Greeting findOne(Long id) {
+        Greeting greeting = greetingRepository.findOne(id);
+        return greeting;
+}
 
-  // find one
-  @Override
-  public Greeting findOne(Long id) {
-    Greeting greeting = greetingMap.get(id);
-    return greeting;
-  }
-
-  // create
-  @Override
-  public Greeting create(Greeting greeting) {
-    Greeting savedGreeting = save(greeting);
-    return savedGreeting;
-  }
-
-  // update
-  @Override
-  public Greeting update(Greeting greeting) {
-      Greeting updatedGreeting = save(greeting);
-      return updatedGreeting;
-  }
-
-  // delete
-  @Override
-  public void delete(Long id) {
-    remove(id);
-  }
-
-  // helper method to create or update greeting objects in the temp data repo- map
-    private static Greeting save(Greeting greeting) {
-      if (greetingMap == null) {
-        greetingMap = new HashMap<Long,Greeting>();
-        nextId = new Long(1);
-      }
-
-      // If save is called to update existing greeting objects
-      if (greeting.getId() != null) {
-        Greeting oldGreetingObj = greetingMap.get(greeting.getId());
-        if (oldGreetingObj == null) {
+// create
+@Override
+public Greeting create(Greeting greeting) {
+        if (greeting.getId() != null) {
+          // cannot be created
           return null;
         }
-        greetingMap.remove(greeting.getId());
-        greetingMap.put(greeting.getId(), greeting);
-        return  greeting;
+        Greeting savedGreeting = greetingRepository.save(greeting);
+        return savedGreeting;
+}
 
-      }
-      // If save is called to create new greeting object
-      greeting.setId(nextId);
-      nextId += 1;
-      greetingMap.put(greeting.getId(), greeting);
-      return greeting;
-    }
+// update
+@Override
+public Greeting update(Greeting greeting) {
+        Greeting persistedObj = greetingRepository.findOne(greeting.getId());
+        if (persistedObj == null) {
+          // object is not persisted previously
+          return null;
+        }
 
-    // helper method to remove data from temp data repo - greetingMap
-    private static boolean remove(Long id ) {
-      Greeting deletedGreetingObj = greetingMap.remove(id);
-      if (deletedGreetingObj == null) {
-        return false;
-      }
-      return true;
-    }
+        Greeting updatedGreeting = greetingRepository.save(greeting);
+        return updatedGreeting;
+}
 
-    // this code block will get executed for the first time JVM loads this class
-    static {
-            Greeting g1 = new Greeting();
-            g1.setText("Hello World!");
-            save(g1);
-
-            Greeting g2 = new Greeting();
-            g2.setText("Hola!");
-            save(g2);
-    }
-
+// delete
+@Override
+public void delete(Long id) {
+        greetingRepository.delete(id);
+}
 
 }

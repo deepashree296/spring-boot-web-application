9082da35e81d7b8ede01fa9b1232e4b90621a427
Deepashree Kedia <kediadeepashree@gmail.com>
2018-04-16 14:35:10 +0530

    committed

--- src/main/java/org/example/ws/batch/GreetingBatchBean.java
+++ src/main/java/org/example/ws/batch/GreetingBatchBean.java
@@ -5,8 +5,10 @@ import org.slf4j.LoggerFactory;
 import java.util.Collection;
 import org.example.ws.model.Greeting;
 import org.example.ws.service.GreetingService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+
 import org.springframework.stereotype.Component;
 
 @Component
